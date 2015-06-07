887431fdaab7cd5c4c8ad9934352360db4437dfc
Marshal Horn <kamocat@gmail.com>
2015-06-06 21:24:51 -0700

    Radio8 now uses radio_mac.h direclty
    but it doesn't work. I need to seperate the pieces and see if they work.

--- apps/radio8/radio8.c
+++ apps/radio8/radio8.c
@@ -4,34 +4,23 @@ This app allows realtime status updates between up to 8 devices.
 
 == Description ==
 
-It keeps an array of the data received, and resends its data if it
-another device didn't recieve it correctly.
-
-Initially, wait to see if other devices are about to send.
-Wait long enough that all the devices have sent packets. Pick
-the next available number. The numbers trjansmit in sequence; each
-has their own timeslot.
-
 Each packet should include:
-Device number
-Mask of which devices it has valid data for
-Status update
-Checksum
-
-If we include the packet length, then this allows the packet sniffer to work.
-I think we can use the radio_queue library for this.
+* Packet length (to allow packet sniffer to work)
+* Device number (0-7)
+* Mask of which devices it has valid data for
+* Status update
+* Checksum
 
 */
 
 /** Dependencies **************************************************************/
 #include <cc2511_map.h>
 #include <board.h>
-#include <random.h>
 #include <time.h>
 
 #include <usb.h>
 #include <usb_com.h>
-#include <radio_queue.h>
+#include "radio8.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -46,22 +35,14 @@ struct rower {
 };
 
 /** Parameters ****************************************************************/
-#define MAX_ROWER_COUNT 8
 int32 CODE param_radio_ID = 1;
-uint8 my_id = param_radio_ID;
-
-int32 CODE param_tx_period = 500;
-
-// radio_channel: See description in radio_link.h.
 
 
 
 
 /** Global Variables **********************************************************/
 
-uint8 devices_seen = 0;	// this is a bitmask of each of the radio IDs that are seen
-struct rower XDATA rowers[MAX_ROWER_COUNT];
-uint8 blink = 0;
+struct rower XDATA my;
 
 /** Functions *****************************************************************/
 
@@ -72,24 +53,32 @@ uint8 blink = 0;
 void updateLeds()
 {
     usbShowStatusWithGreenLed();
-
-    LED_YELLOW(radioQueueRxCurrentPacket());
-	
-    LED_RED(blink);
+    LED_RED_TOGGLE();
 }
 
+uint8 devices_seen() {
+	uint8 seen = 0;
+	uint8 id;
+	uint8 i = 0;
+	for(; i < RADIO_COUNT; ++i) {
+		id = store[i][1];
+		if( i == id )
+			seen |= 1<<id;
+	}
+	return seen;
+}
 
 void build_packet( void ) {
-	uint8 XDATA * buffer = radioQueueTxCurrentPacket();
+	uint8 XDATA * buffer = tx_next_buffer();
 	buffer[0] = 9;		//Size of packet
-	buffer[1] = my_id;
-	buffer[2] = devices_seen;
+	buffer[1] = my_ID;
+	buffer[2] = devices_seen(); // update this to determine devices seen
 	
 	#if 1
 	buffer += 3; // now we write the 16 bit values
-	write_int16( rowers[my_id].torque, buffer );
-	write_int16( rowers[my_id].speed, buffer );
-	write_int16( rowers[my_id].damper, buffer );
+	write_int16( my.torque, buffer );
+	write_int16( my.speed, buffer );
+	write_int16( my.damper, buffer );
 	#else
 	buffer[3] = send.torque >> 8;
 	buffer[4] = send.torque & 0xFF;
@@ -99,59 +88,33 @@ void build_packet( void ) {
 	buffer[8] = send.damper & 0xFF;
 	#endif
 	
-	radioQueueTxSendPacket();
+	tx_packet = buffer;
 	
 	return;
 }
 
-void store_packet( void ) {
-	if( radioQueueTxQueued() ){
-		uint8 XDATA * buffer = radioQueueRxCurrentPacket();
-		uint8 id = buffer[1] & 0x07;
-		devices_seen |= 1<<id;
-		rowers[id].timestamp = getMs();
-		rowers[id].torque = read_int16(buffer);
-		rowers[id].speed = read_int16(buffer);
-		rowers[id].damper = read_int16(buffer);
-		
-		radioQueueRxDoneWithPacket();
-	}
-	
-	return;
-}
-
-
-void main( void ) {
-	
-	uint8 i = 0;	// this is just for doling out processor time
-	char * sample = "TqSpDm";
-	int32 oldtime;
+void main( void ) {	
+	uint8 XDATA i = 0; // just for doling out processor time
 	
 	systemInit();
 	usbInit();
-	radioQueueInit();
+	radioMacInit();
+	radioInitAddendum();
 	
-	
-	rowers[my_id].torque = read_int16(sample);
-	rowers[my_id].speed = read_int16(sample);
-	rowers[my_id].damper = read_int16(sample);
+	my_ID = param_radio_ID;
+	my.torque = ('T' << 8) | 'q';
+	my.speed = ('S'<<8) | 'p';
+	my.damper = ('D' << 8) | 'm';
 	
 	
 	while(1){
 		++i;
 		
-        boardService();
-        updateLeds();
-        usbComService();
+		boardService();
+		updateLeds();
+		usbComService();
 		
+		build_packet();
 		
-		if( ( getMs() - oldtime) > param_tx_period ){ // once per period
-			oldtime = getMs();
-			blink = !blink;
-			build_packet();
-		}
-		store_packet();
 	}
-	
-	return;
 }
--- apps/radio8/radio8.h
+++ apps/radio8/radio8.h
@@ -1,38 +1,104 @@
 #include <radio_mac.h>
+#include <board.h> // for LEDs
+#include <cc2511_map.h>
+#include <dma.h>
+#include <radio_registers.h>
 
-#defiine RADIO_COUNT 8
-#define MAX_TIMEOUT 9
+/**
+==Notes==
+This uses the yellow LED to show packet transmission
 
-#define next_index() 
+We need to take control over the calibration, because I want to make
+use of the rx_timout for packet scheduling.
+
+*/
+
+#define RADIO_COUNT 8
+#define MAX_TIMEOUT 9 // We want to send at least every 10ms
+#define MAX_PACKET_LEN 20
+#define RX_BUFFER_COUNT 16
+#define TX_BUFFER_COUNT 3
+
+
+/** Global Variables */
+int32 CODE param_radio_channel = 128; // 0 - 255. Seperate by 2 to avoid crosstalk
+
+uint8 XDATA rx_buffer[RX_BUFFER_COUNT][MAX_PACKET_LEN];
+int8 XDATA rx_buffer_index;
+uint8 XDATA tx_buffer[TX_BUFFER_COUNT][MAX_PACKET_LEN];
+int8 XDATA tx_buffer_index;
+
+uint8 XDATA my_ID;
+uint8 XDATA * rx_packet;
+uint8 XDATA * tx_packet;
+uint8 XDATA * store[RADIO_COUNT];
+
+#if 1
+#define rx_next_buffer() rx_buffer[rx_buffer_index = (rx_buffer_index ? rx_buffer_index : RX_BUFFER_COUNT) - 1 ]
+#define tx_next_buffer() tx_buffer[tx_buffer_index = (tx_buffer_index ? tx_buffer_index : TX_BUFFER_COUNT) - 1 ]
+#else // the function-based equivelent, for debugging
+uint8 XDATA * rx_next_buffer() {
+	rx_buffer_index = (rx_buffer_index ? rx_buffer_index : RX_BUFFER_COUNT) - 1;
+	return rx_buffer[rx_buffer_index];
+}
+uint8 XDATA * tx_next_buffer() {
+	tx_buffer_index = (tx_buffer_index ? tx_buffer_index : TX_BUFFER_COUNT) - 1;
+	return tx_buffer[tx_buffer_index];
+}
+#endif
+
+void radioInitAddendum() {
+	//MCSM0 = 0x04;	// FS_AUTOCAL = 0, so we need to calibrate the oscillator manually
+	rx_packet = rx_next_buffer();
+	tx_packet = tx_next_buffer();	// this is full of junk data, but we can't allow a seg fault
+	CHANNR = param_radio_channel;
+	PKTLEN = MAX_PACKET_LEN;
+}
+
 
 /* This function is declared in radio_mac.h, but needs to be defined here */
-radioMacEventHandler( uint8 event) {
-switch( event ) {
-	default:
-	case RADIO_MAC_EVENT_STROBE: //this function was manually entered
-	case RADIO_MAC_EVENT_TX: //Packet successfully sent
-		radioMacRx( rx_packet, MAX_TIMEOUT ); // we want to send every 10ms
-		break;
-
-	case RADIO_MAC_EVENT_RX: //Packet recieved
-		If( bad CRC){
-			calibrate radio, then call radioMacRx()
+void radioMacEventHandler( uint8 event) {
+	uint8 rx_ID;
+	uint8 rx_timeout;
+	
+	switch( event ) {
+		case RADIO_MAC_EVENT_RX: //Packet recieved
+			if( !radioCrcPassed() ){
+				//calibrate radio, then call radioMacRx()
+				LED_RED(1);
+				//RFST=SCAL; //This may take more work than I want
+				radioMacRx( rx_packet, MAX_TIMEOUT );
 			
-		}else{
-			rx_ID = packet[1];
-			store[ID] = rx_packet; // Faster than copying, but could result in two IDs pointing to the same data
-			rx_packet = next_index();
-			rx_timeout = (my_ID - rx_ID - 1) % RADIO_COUNT;
-			
-			if( rx_timeout == 0 ){
+			}else if( rx_packet[1] >= RADIO_COUNT ) {
+				// Something is wrong. Force your way in anyway.
 				radioMacTx( tx_packet );
-			}else{
-				radioMacRx( rx_packet, rx_timeout );
+				//LED_YELLOW(1);
+			}else{ // YAY! The CRC matched. Store the data.
+				LED_RED(0);
+				rx_ID = rx_packet[1];
+				store[rx_ID] = rx_packet; // WARNING: could result in two IDs pointing to the same data
+				rx_packet = rx_next_buffer();
+				rx_timeout = (my_ID > rx_ID) ? (my_ID - rx_ID - 1) : (my_ID + RADIO_COUNT - rx_ID);
+				
+				if( rx_timeout == 0 ){ // We are next! Send away.
+					radioMacTx( tx_packet );
+					//LED_YELLOW(1);
+				}else{ // Wait for more devices. It's not our turn yet
+					radioMacRx( rx_packet, rx_timeout );
+				}
 			}
-		}
-		break;
+			break;
+			
+		case RADIO_MAC_EVENT_RX_TIMEOUT: // We have waited for others to send, and now it is our turn!
+			radioMacTx( tx_packet );
+			//LED_YELLOW(1);
+			break;
 		
-	case RADIO_MAC_EVENT_RX_TIMEOUT:
-		radioMacTx( tx_packet );
-		break;
+		default:
+		//case RADIO_MAC_EVENT_STROBE:
+		case RADIO_MAC_EVENT_TX: //Packet successfully sent
+			radioMacRx( rx_packet, MAX_TIMEOUT );
+			LED_YELLOW_TOGGLE();
+			break;
+	}
 }
