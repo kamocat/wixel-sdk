f5a31ee7d95310b1c4cc65c3bd714dc59a644ab4
Marshal Horn <kamocat@gmail.com>
2015-07-29 20:01:41 -0700

    radio8 now recognizes radio IDs
    It stores them according to their number, and will print on request.

--- apps/radio8/radio8.c
+++ apps/radio8/radio8.c
@@ -22,6 +22,7 @@ This app allows realtime status updates between up to 8 devices.
 #define MAX_PACKET_LEN 30
 #define RX_ARRAY_SIZE 4
 #define RX_ARRAY_MAX (RX_ARRAY_SIZE - 1)
+#define RADIO_COUNT 10
 
 int32 CODE param_radio_channel = 128; // 0 - 255. Seperate by 2 channels to avoid crosstalk
 
@@ -29,6 +30,8 @@ uint8 XDATA * rx_packet;
 uint8 XDATA rx_array[RX_ARRAY_SIZE][MAX_PACKET_LEN + 4];// room to store CRC
 uint8 DATA rx_array_front = 0;
 uint8 DATA rx_array_len = 0;
+
+uint8 XDATA packet_storage[RADIO_COUNT][MAX_PACKET_LEN];
 
 /** Functions *****************************************************************/
 
@@ -59,11 +62,14 @@ uint8 XDATA * dequeue_rx_packet(){
 
 
 uint8 XDATA * enqueue_rx_packet() {
-	uint8 tmp = rx_array_front;
-	if( rx_array_len < RX_ARRAY_SIZE ) { // If there's no room, overwrite the most recent
+	uint8 tmp;
+	/* We have to leave one space so that the main() loop can process data without fear of it
+	being overwritten. If that's all that's left, then we overwrite the most recent data, because
+	returning a null pointer risks a seg fault. */
+	if( rx_array_len < RX_ARRAY_MAX ) {
 		++rx_array_len;
 	}
-	tmp += rx_array_len;
+	tmp = rx_array_len + rx_array_front;
 	if( tmp > RX_ARRAY_MAX ) {
 		tmp -= RX_ARRAY_SIZE;
 	}
@@ -71,7 +77,42 @@ uint8 XDATA * enqueue_rx_packet() {
 }
 		
 
+void copy_to_storage( uint8 * XDATA packet ){
+	uint8 DATA i = 0;
+	if( packet == 0 ){ // invalid pointer
+		return;
+	}
+	if( packet[0] > MAX_PACKET_LEN ){
+		packet[0] = MAX_PACKET_LEN; // Don't discard the data, but we can't hold it all.
+	}
+	if( packet[1] >= RADIO_COUNT ){
+		return;	// Invalid radio ID
+	}
+	/* At this point, the packet may have corrupted data,
+	   but at least it won't break our storage. */
+	for( ; i <= packet[0]; ++i ){
+		packet_storage[packet[1]][i] = packet[i];
+	}
+	return;
+}
 
+void print_packet( uint8 * XDATA packet ){
+	uint8 PDATA avail = usbComTxAvailable();
+	uint8 PDATA len;
+	if( packet == 0){
+		return;
+	}
+	if( packet[0] == 0 ){ // If we haven't written data there yet
+		return;
+	}
+	len = packet[0] - 1;
+	if( len > avail ){
+		len = avail;
+	}
+	usbComTxSend(&packet[2], len); //Start after the radio ID
+	
+	return;
+}
 
 /* This function is declared in radio_mac.h, but needs to be defined here */
 void radioMacEventHandler( uint8 event) {
@@ -85,21 +126,45 @@ void radioMacEventHandler( uint8 event) {
 
 void main( void ) {	
 	uint8 XDATA * rx_processing;
-	char XDATA * print_string[20];
+	char DATA command;
+	uint8 DATA j;
 	CHANNR = param_radio_channel;
 	PKTLEN = MAX_PACKET_LEN;
 	systemInit();
 	usbInit();
 	radioMacInit();
 	radioMacStrobe();
+	
+	/* Clear the storage array */
+	for( j = 0; j < RADIO_COUNT; ++j ){
+		packet_storage[j][0] = 0;
+	}
 
 	while(1){
 		usbComService();
 		
-		if( RX_AVAILABLE() && (usbComTxAvailable() > 5 )){
+		if( RX_AVAILABLE() ){
 			rx_processing = dequeue_rx_packet();
 			
-			usbComTxSend((rx_processing+2), 5);
+			copy_to_storage( rx_processing );
+		}
+		if( usbComRxAvailable() ){
+			command = usbComRxReceiveByte();
+			if( (command <= '9') && (command >= '0') ){
+				command -= '0';
+				if( command < RADIO_COUNT ){
+					print_packet(packet_storage[command]);
+				}
+			} else if( command == 'a' ) {
+				for( j = 0; j < RADIO_COUNT; ++j ){
+					print_packet(packet_storage[j]);
+				}
+				command = '\r';
+			} if( command == '\r' ){
+				if( usbComTxAvailable() >= 2 ){
+					usbComTxSend( (uint8 XDATA *)"\r\n", 2 );
+				}
+			}
 		}
 	}
 }
